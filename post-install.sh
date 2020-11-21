5a9f686a4eb7a7e5ffdea26aa601395ebb9d29f0
davbianchi <davideb1912@gmail.com>
2020-11-21 17:13:59 +0100

    Minor shits

--- post-install.sh
+++ post-install.sh
@@ -10,7 +10,10 @@ sudo reflector --verbose -c Germany -c France -c Italy -l 200 -p http --sort rat
 sudo pacman -Syyu
 
 # start installing packages
-sudo pacman -S networkmanager xfce4-desktop git xf86-video-amdgpu xorg-server xorg-xinit ttf-dejavu ttf-fira-mono zip unzip unrar pulseaudio pamixer telegram-desktop python python-pip openssh xorg-xrandr nemo man mesa vulkan-radeon libva-mesa-driver mumble mesa-vdpau discord bind-tools gnome-keyring ccache ttf-opensans pavucontrol rclone ufw vim texlive-most biber texstudio code thunderbird papirus-icon-theme papirus-folders hwlock tor torbrowser-launcher network-manager-applet bash-autocompletion mousepad
+sudo pacman -S networkmanager xfce4-desktop git xf86-video-amdgpu xorg-server xorg-xinit ttf-dejavu ttf-fira-mono zip unzip unrar pulseaudio\
+pamixer telegram-desktop python python-pip openssh xorg-xrandr nemo man mesa vulkan-radeon libva-mesa-driver mumble mesa-vdpau discord bind-tools\
+gnome-keyring ccache ttf-opensans pavucontrol rclone ufw vim texlive-most biber texstudio code thunderbird papirus-icon-theme papirus-folders\
+hwlock tor torbrowser-launcher network-manager-applet bash-autocompletion mousepad
 
 # install yay
 cd
@@ -21,4 +24,4 @@ cd
 rm -rf $HOME/yay
 
 # install packages on aur
-yay -S spotify torsocks ttf-meslo noto-fonts-main
+yay -S spotify torsocks ttf-menlo-powerline-git noto-fonts-main
