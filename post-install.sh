4729f61a96f39de843e7dd92f4801042802aa05c
davbianchi <davideb1912@gmail.com>
2020-12-26 13:44:23 +0100

    Added cups,generic drivers for Canon printers and printer wizard

--- post-install.sh
+++ post-install.sh
@@ -10,10 +10,15 @@ sudo reflector --verbose -c Germany -c France -c Italy -l 200 -p http --sort rat
 sudo pacman -Syyu
 
 # start installing packages
-sudo pacman -S networkmanager xfce4-desktop git xf86-video-amdgpu xorg-server xorg-xinit ttf-dejavu ttf-fira-mono zip unzip unrar pulseaudio \
+sudo pacman -S networkmanager git xf86-video-amdgpu xorg-server xorg-xinit ttf-dejavu ttf-ubuntu-font-family zip unzip unrar pulseaudio \
 pamixer telegram-desktop python python-pip openssh xorg-xrandr nemo man mesa vulkan-radeon libva-mesa-driver mumble mesa-vdpau discord bind-tools \
-gnome-keyring ccache ttf-opensans pavucontrol rclone ufw vim texlive-most biber texstudio code thunderbird papirus-icon-theme papirus-folders \
-hwlock tor torbrowser-launcher network-manager-applet bash-autocompletion mousepad gvfs gvfs-mtp
+gnome-keyring ccache ttf-opensans pavucontrol rclone ufw vim code thunderbird papirus-icon-theme papirus-folders \
+hwlock tor torbrowser-launcher network-manager-applet bash-autocompletion mousepad gvfs gvfs-mtp pasystray \
+system-config-printer cups
+
+# Enabling cups.service for printer (Canon printer)
+sudo systemctl enable cups.service
+sudo systemctl start cups.service
 
 # install yay
 cd
@@ -24,4 +29,4 @@ cd
 rm -rf $HOME/yay
 
 # install packages on aur
-yay -S spotify torsocks ttf-menlo-powerline-git noto-fonts-main
+yay -S spotify torsocks ttf-menlo-powerline-git cnijfilter2
